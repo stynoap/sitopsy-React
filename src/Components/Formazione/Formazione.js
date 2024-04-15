9a9e1cb8e29ce60eeec2a81112aad56c528e2162
stynoap <giulia.pinna47@tiscali.it>
2024-04-15 17:58:35 +0200

    modifiche stili css

--- src/Components/Formazione/Formazione.js
+++ src/Components/Formazione/Formazione.js
@@ -4,11 +4,11 @@ const Formazione = () => {
         <section className='la-mia-formazione' id='Formazione'>
             <h2>Formazione</h2>
             <ul className='descrizione'>
-                <li>2024 - Iscritta al primo anno della scuola di specializzazione in Psicoterapia presso l'Istituto Atanor, ad indirizzo Analitico-Archetipico</li>
-                <li>2023 - Tirocinio obbligatorio post-Laurea presso la Scuola Romana di Psicoterapia Familiare ad indirizzo Simbolico-Esperienziale</li>
-                <li>2021 - Laurea Magistrale in Psicologia dinamica e dello sviluppo LM-51 presso L'Università degli Studi di Cagliari </li>
-                <li>2019 - Laurea Triennale in Scienze e Tecniche Psicologiche presso l'Università degli Studi di Cagliari</li>
-                <li>2016 - Diploma di Scienze Umane presso l'Istituto magistrale Carlo B. Di Vesme di Iglesias </li>
+                <li><b>2024</b> - Iscritta al primo anno della scuola di specializzazione in Psicoterapia presso l'Istituto Atanor, ad indirizzo Analitico-Archetipico</li>
+                <li> <b>2023</b> - Tirocinio obbligatorio post-Laurea presso la Scuola Romana di Psicoterapia Familiare ad indirizzo Simbolico-Esperienziale</li>
+                <li><b>2021</b> - Laurea Magistrale in Psicologia dinamica e dello sviluppo LM-51 presso L'Università degli Studi di Cagliari </li>
+                <li><b>2019</b> - Laurea Triennale in Scienze e Tecniche Psicologiche presso l'Università degli Studi di Cagliari</li>
+                <li><b>2016</b> - Diploma di Scienze Umane presso l'Istituto magistrale Carlo B. Di Vesme di Iglesias </li>
             </ul>
 
 
--- src/Components/Servizi/Servizi.js
+++ src/Components/Servizi/Servizi.js
@@ -8,8 +8,7 @@ const Servizi = () => {
         <section className="i-miei-servizi" id='Servizi'>
             <h2>Servizi</h2>
             <div className='descrizione'>
-                <p>Offro consulenze e sostegno psicologico online attraverso le principali piattaforme di videoconferenza quali Zoom, Google Meet e attraverso Skype. Per le sedute in presenza
-                    mi sto organizzando per ricevere sul territorio di Perugia. </p>
+                <p>Offro consulenze e sostegno psicologico online attraverso le principali piattaforme di videoconferenza quali Zoom, Google Meet e attraverso Skype. </p>
                
             </div>
 
@@ -18,30 +17,31 @@ const Servizi = () => {
 
             <div className="row g-4 py-2 px-5 row-cols-1 row-cols-lg-3" id="servizi-icone">
                 <div className="feature col">
-                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-center bg bg-gradient fs-5 mb-5 icona">
+                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-center bg bg-gradient fs-9 mb-5 icona">
                         <BiMessageSquare size="3em" />
                     </div>
                     <h3 className="fs-2 text-body-emphasis roboto-font" style={{ color: '#8C83B0' }}>Sostengo psicologico</h3>
                     <p>Servizio attivo con adolescenti, adulti e giovani adulti</p>
                 </div>
                 <div className="feature col">
-                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-center text-dark bg-gradient fs-5 mb-5 icona">
+                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-center text-dark bg-gradient fs-9 mb-5 icona">
                         <BiBook size="3em" />
                     </div>
-                    <h3 className="fs-2 text-body-emphasis roboto-font" style={{ color: '#8C83B0' }}>Orientamento professionale</h3>
+                    <h3 className="fs-2 text-body-emphasis roboto-font" style={{ color: '#8C83B0' }}>Orientamento</h3>
                     <p>In percorsi scolastici, lavorativi e relative fasi di transizione</p>
                 </div>
                 <div className="feature col">
-                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-center text-dark bg-gradient fs-5 mb-5 icona">
+                    <div className="feature-icon d-inline-flex align-items-center justify-content-center text-center text-dark bg-gradient fs-9 mb-5 icona">
                         <BiMoon size="3em" />
                     </div>
                     <h3 className="fs-2 text-body-emphasis roboto-font" style={{ color: '#8C83B0' }} id="titolo-servizio">Disponibilità notturna</h3>
-                    <p>Possibilità di avere sostegno psicologico ovunque tu sia, nella fascia serale e anche notturna. </p>
+                    <p>Possibilità di avere sostegno psicologico ovunque tu sia, nella fascia serale e anche notturna*. </p>
                 </div>
             </div>
 <br></br>
 <br></br>
             <h5>Il colloquio psicologico ha una durata di circa 60 minuti ed il costo è di 40€.</h5>
+            <h6>*Il servizio notturno è disponibile sino alle 02.00 a.m.</h6>
         </section>
     )
 }
